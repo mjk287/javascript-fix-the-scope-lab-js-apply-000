20f797c6c36b7aa26b652f31373494b5721cc330
Myung Kim <mjk287@cornell.edu>
2018-12-17 19:51:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,7 +25,5 @@ var funkyFunction = function() {
   }
 }
 
-// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE: you only need to modify the code below this line.
 var getInnerFunction = funkyFunction()
 var theFunk = getInnerFunction()
